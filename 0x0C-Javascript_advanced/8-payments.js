e678ffb38db19c25ce5dc99069c971730d93ae64
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-02-17 01:32:50 -0500

    Stack order and setTimeout

---
+++ 0x0C-Javascript_advanced/8-payments.js
@@ -0,0 +1,13 @@
+let processPayment = (amout) => console.log(`Collecting payment of ${amout}`)
+
+let processOrder = (orderId, amount) => {
+    console.log(`${orderId} is being processed`)
+    processPayment(amount)
+    console.log(`${orderId} has been fully processed`)
+}
+
+console.log(`Processing orders`)
+processOrder(12321, 10.99)
+processOrder(12322, 12.99)
+processOrder(12323, 15.0)
+console.log(`All the orders have been processed`)
